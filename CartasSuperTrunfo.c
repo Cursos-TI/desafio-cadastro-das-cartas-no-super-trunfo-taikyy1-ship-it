49f4be52931346c3ea07c24e16452041f21478d4
ORSTED <taikyy1@gmail.com>
2025-09-06 20:49:00 +0000

    VARIAVEIS

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+int main(){
+    //CEARÁ COM 148 894,442 km² DE AREA
+    //POPULAÇÃO EM 2025 9.268.836 habitantes 
+    //VALOR DO PIB EM 2025 4,18%
+    //6 PONTOS TURISTICOS
+     
+    //AMAZONAS COM 1.571.000 km² DE AREA
+    //POPULAÇÃO EM 2025 4.281.209 milhões
+    //VALOR DO PIB EM 2025 9,86% 
+    //10 PONTOS TURISTICOS
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+
+
+
+
+
 
     return 0;
 }
