f7b7544426a17ed055466a636395d36886d1b0a5
ORSTED <taikyy1@gmail.com>
2025-09-08 19:49:06 +0000

    Terminei as duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,76 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    //CEARÁ COM 148 894,442 km² DE AREA
-    //POPULAÇÃO EM 2025 9.268.836 habitantes 
-    //VALOR DO PIB EM 2025 4,18%
-    //6 PONTOS TURISTICOS
-     
-    //AMAZONAS COM 1.571.000 km² DE AREA
-    //POPULAÇÃO EM 2025 4.281.209 milhões
-    //VALOR DO PIB EM 2025 9,86% 
-    //10 PONTOS TURISTICOS
+typedef struct {
+    char codigo[4];
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int turismo;
+} Carta;
 
-    // "" ; : ''
-    char estado, estado2;
-    char codigo[3], condigo2[3];
-   // char nome_cidade[50], nome_cidade2[50];
-    int populacao, populacao2;
-    float area, area2;
-    float pib, pib2;
-    int turismo, turismo2;
-    
-    char print_estado[10] = "Estado: ";
-    char print_codigo[10] = "Codigo: ";
-    char print_nome_cidade[20] = "Nome Cidade: ";
-    char print_populacao[50] = "População: ";
-    char print_area[50] = "Área: ";
-    char print_pib[50] = "PIB: ";
-    char print_turismo[50] = "Número de Pontos Turísticos: ";
+int main() {
+    Carta cartas[] = {
+        {"A01", "Ceara",    9268836, 148894.442, 4.18, 6},
+        {"B01", "Amazonas", 4281209, 1571000.0,  9.86, 10}
+    };
+    int num_cartas = sizeof(cartas)/sizeof(cartas[0]);
 
+    char estado;
+    printf("\nEstado (A para Ceara, B para Amazonas): ");
+    scanf(" %c", &estado); // espaço antes do %c para ignorar '\n'
 
-    printf("CARTA 1\n");
-
-    printf("\n%s", print_estado);
-    scanf("%s", estado);
-
-
-    char nome_cidade[50] = "Ceara";
-    char codigo[3] = "A01";
-    populacao = 92680836;
-    area = 148.894442;
-    pib = 4.18;
-    turismo = 6;
-
-    char nome_cidade2[50] = "Ceara";
-    char codigo[3] = "A01";
-    populacao = 92680836;
-    area = 148.894442;
-    pib = 4.18;
-    turismo = 6;
-
-    printf("\n%s %s", print_codigo, codigo);
-
-    printf("\n%s %s", print_nome_cidade, nome_cidade);
-
-    printf("\n%s %d", print_populacao, populacao);
-
-    printf("\n%s %f", print_area, area);
-  
-    printf("\n%s %f", print_pib, pib);
-    
-    printf("\n%s %d", print_turismo, turismo);
-    
-   
-   
-    
-
-
-
-
-
-
-
+    int encontrada = 0;
+    for (int i = 0; i < num_cartas; i++) {
+        if ((estado == 'A' && strcmp(cartas[i].codigo, "A01") == 0) ||
+            (estado == 'B' && strcmp(cartas[i].codigo, "B01") == 0)) {
+            printf("\nCARTA %d\n", i+1);
+            printf("Codigo: %s\n", cartas[i].codigo);
+            printf("Nome Cidade: %s\n", cartas[i].nome);
+            printf("População: %d\n", cartas[i].populacao);
+            printf("Área: %.3f km²\n", cartas[i].area);
+            printf("PIB: %.2f%%\n", cartas[i].pib);
+            printf("Número de Pontos Turísticos: %d\n", cartas[i].turismo);
+            encontrada = 1;
+            break;
+        }
+    }
+    if (!encontrada) {
+        printf("CARTA NÃO EXISTE\n");
+    }
     return 0;
 }
